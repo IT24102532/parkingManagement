c73b5f063ecf9c07a89cf7c36c1025dc5bb4b042
Kavindu Nirmal <65773645+KavinduNirmal@users.noreply.github.com>
2025-03-22 18:41:33 +0530

    Moved the json files

---
+++ src/main/java/lk/sliit/parkingmanagement/oopapp/Customer.java
@@ -0,0 +1,40 @@
+package lk.sliit.parkingmanagement.oopapp;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer extends User {
+    @SerializedName("carType")
+    private String carType;
+    @SerializedName("licensePlate")
+    private String licensePlate;
+    @SerializedName("paymentDetails")
+    private PaymentDetails paymentDetails;
+
+    public Customer(String username, String email, String userId, String hashedPassword, String carType, String licensePlate, PaymentDetails paymentDetails) {
+        super(username, email, userId, hashedPassword, "user", new ArrayList<>());
+        this.carType = carType;
+        this.licensePlate = licensePlate;
+        this.paymentDetails = paymentDetails;
+    }
+
+    public String getCarType() { return carType; }
+    public void setCarType(String carType) { this.carType = carType; }
+
+    public String getLicensePlate() { return licensePlate; }
+    public void setLicensePlate(String licensePlate) { this.licensePlate = licensePlate; }
+
+    public PaymentDetails getPaymentDetails() { return paymentDetails; }
+    public void setPaymentDetails(PaymentDetails paymentDetails) { this.paymentDetails = paymentDetails; }
+
+    @Override
+    public String toString() {
+        return super.toString() + " RegularUser{" +
+                "carType='" + carType + '\'' +
+                ", licensePlate='" + licensePlate + '\'' +
+                ", paymentDetails=" + paymentDetails +
+                '}';
+    }
+}
---
+++ src/main/java/lk/sliit/parkingmanagement/oopapp/PaymentDetails.java
@@ -0,0 +1,25 @@
+package lk.sliit.parkingmanagement.oopapp;
+
+public class PaymentDetails {
+    private String cardNumber;
+    private String expiry;
+
+    public PaymentDetails(String cardNumber, String expiry) {
+        this.cardNumber = cardNumber;
+        this.expiry = expiry;
+    }
+
+    public String getCardNumber() { return cardNumber; }
+    public void setCardNumber(String cardNumber) { this.cardNumber = cardNumber; }
+
+    public String getExpiry() { return expiry; }
+    public void setExpiry(String expiry) { this.expiry = expiry; }
+
+    @Override
+    public String toString() {
+        return "PaymentDetails{" +
+                "cardNumber='" + cardNumber + '\'' +
+                ", expiry='" + expiry + '\'' +
+                '}';
+    }
+}
