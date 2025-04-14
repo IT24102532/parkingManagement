fbfae36738ad8d779cefe390612cfa60a1515af2
Kavindu Nirmal <65773645+KavinduNirmal@users.noreply.github.com>
2025-04-14 09:19:19 +0530

    Update InstaSlot.java

--- src/main/java/lk/sliit/parkingmanagement/oopapp/model/InstaSlot.java
+++ src/main/java/lk/sliit/parkingmanagement/oopapp/model/InstaSlot.java
@@ -1,20 +1,34 @@
 package lk.sliit.parkingmanagement.oopapp.model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InstaSlot extends ParkingSlot {
-    private double pricePerHour;
+    // Attributes
+    @SerializedName("price")
+    private double price;
+    @SerializedName("max_hours")
     private int maxDurationHours = 1;
+    @SerializedName("overstay_charge")
     private double overStayCharge;
+    @SerializedName("booked_times")
+    private List<String> bookedTimes;
 
+    // Constructors
     public InstaSlot() {}
-
-    public InstaSlot(double pricePerHour, int maxDurationHours, double overStayCharge) {
-        this.pricePerHour = pricePerHour;
+    public InstaSlot(String slotId, String location, String managerId, boolean lotType, String locationName, boolean isAvailable, double price, int maxDurationHours, double overStayCharge) {
+        super(slotId, location, managerId, lotType, locationName, isAvailable);
+        this.price = price;
         this.maxDurationHours = maxDurationHours;
         this.overStayCharge = overStayCharge;
+        this.bookedTimes = new ArrayList<>();
     }
 
-    public double getPricePerHour() {return pricePerHour;}
-    public void setPricePerHour(double pricePerHour) {this.pricePerHour = pricePerHour;}
+    // Getters and Setters
+    public double getPrice() {return price;}
+    public void setPrice(double price) {this.price = price;}
 
     public int getMaxDurationHours() {return maxDurationHours;}
     public void setMaxDurationHours(int maxDurationHours) {this.maxDurationHours = maxDurationHours;}
@@ -22,12 +36,17 @@ public class InstaSlot extends ParkingSlot {
     public double getOverStayCharge() {return overStayCharge;}
     public void setOverStayCharge(double overStayCharge) {this.overStayCharge = overStayCharge;}
 
+    public List<String> getBookedTimes() {return bookedTimes;}
+    public void setBookedTimes(List<String> bookedTimes) {this.bookedTimes = bookedTimes;}
+
+    // Overrides
     @Override
     public String toString() {
         return super.toString() + " InstaSlot{" +
-                "pricePerHour=" + pricePerHour +
+                "price=" + price +
                 ", maxDurationHours=" + maxDurationHours +
                 ", overStayCharge=" + overStayCharge +
+                ", bookedTimes=" + bookedTimes +
                 '}';
     }
 }
