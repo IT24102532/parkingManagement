098863e0ada0f6b7ed1d20b08f02d61fa837a63a
IT24104332 <IT24104332@my.sliit.lk>
2025-05-20 00:34:54 +0530

    Documentation

--- src/main/java/lk/sliit/parkingmanagement/oopapp/api/LongTermSlotsGetServlet.java
+++ src/main/java/lk/sliit/parkingmanagement/oopapp/api/LongTermSlotsGetServlet.java
@@ -14,13 +14,23 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.logging.Logger;
 
+/*
+*Servlet designed to retrieve detailed info about Long term parking slots
+* param id - id of the parking slots to be retrieved
+*
+* this servlet reads data from parkingSlot using provided id
+*/
+
 @WebServlet(name = "LongTermSlotsGetServlet", value = "/get/slot/id")
 public class LongTermSlotsGetServlet extends HttpServlet {
+    //DAO Access
     private final Logger LOGGER = Logger.getLogger(LongTermSlotsGetServlet.class.getName());
     private final ParkingSlotDao parkingSlotDao = new ParkingSlotDaoImpl();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String id = request.getParameter("id");
+        //create  a Gson with a custom adapter for LocalDateTime
         Gson gson = new GsonBuilder()
                 .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
                 .create();
@@ -28,6 +38,7 @@ public class LongTermSlotsGetServlet extends HttpServlet {
         try {
             slot = parkingSlotDao.getById(id);
         } catch (Exception e) {
+            //Handle server side exceptions
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             response.setContentType("application/json");
             response.setCharacterEncoding("UTF-8");
@@ -35,6 +46,7 @@ public class LongTermSlotsGetServlet extends HttpServlet {
             return;
         }
 
+        //If no slot found to the given id, return error 404
         if (slot == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             response.setContentType("application/json");
@@ -42,6 +54,7 @@ public class LongTermSlotsGetServlet extends HttpServlet {
             response.getWriter().write("{\"success\": false, \"error\": \"Slot not found!\"}");
             return;
         }
+        //if the slot is successfully retrieved,return its details as a JSON response 
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         response.getWriter().print(gson.toJson(slot));
